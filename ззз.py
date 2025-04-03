e455e95ac763da0a9ab85cf75007bac11e6a7a8c
levkozurev <lev453957@gmail.com>
2025-04-03 11:50:04 +0700

    Update ззз.py

--- ззз.py
+++ ззз.py
@@ -1,22 +1,10 @@
-import flet as ft
+class Object:
+    def __init__(self, car_brand, model):
+        self.car_brand = car_brand
+        self.model = model
 
-def main(page: ft.Page):
-    page.title = "Заметки с счетчиком"
+mazda= Object('Mazda',CX5)
+bently = Object('Bently',Continental_GT)
 
-    notes = ft.Column()
-    input_field = ft.TextField(label="Введите заметку")
-    counter = ft.Text("Количество заметок: 0")  # Счетчик заметок
-
-    def add_note(e):
-        if input_field.value:
-            notes.controls.append(ft.Text(input_field.value))
-            input_field.value = ""
-            # Обновляем счетчик
-            counter.value = f"Количество заметок: {len(notes.controls)}"
-            page.update()
-
-    button = ft.ElevatedButton("Добавить заметку", on_click=add_note)
-
-    page.add(input_field, button, counter, notes)  # Добавляем счетчик на страницу
-
-ft.app(target=main)
+print(mazda.model)
+print(bently.model)
